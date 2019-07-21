734572da51389c6e9672d61cdfba190f5d438956
mayuxian <mayuxian@agree.com.cn>
2019-07-21 10:43:33 +0800

    提出初始化项目

---
+++ AutoUpdater/AutoUpdater.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AutoUpdater
+{
+    public class AutoUpdater
+    {
+    }
+}
---
+++ MultiPlatformUpdater/AutoUpdater.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace AutoUpdater
+{
+    public class AutoUpdater
+    {
+      
+    }
+}
