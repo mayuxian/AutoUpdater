85721a69b4aec4ffdaef836708c67bcab5b885e5
mayuxian <415705683@qq.com>
2019-07-28 22:38:19 +0800

    1.更新设计说明 2.添加静默和交互更新的更新器

---
+++ AutoUpdater/Updaters/InteractionUpdater.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoUpdater.Updaters
+{
+    /// <summary>
+    /// 交互更新器
+    /// </summary>
+    /// <remarks>
+    /// 默认更新器
+    /// </remarks>
+    internal class InteractionUpdater : IUpdateFlow
+    {
+        public Task<bool> ApplyUpdate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> CheckUpdate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> CloseApp(string processName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CommunicationExceptionHandle()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DownloadFileExceptionHandle()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> StartUpate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UnhandlePlatformExceptionHandle()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> WaitAppExit()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AutoUpdater/Updaters/SlientUpdater.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoUpdater.Updaters
+{
+    internal class SlientUpdater : IUpdateFlow
+    {
+        public Task<bool> ApplyUpdate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> CheckUpdate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> CloseApp(string processName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CommunicationExceptionHandle()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DownloadFileExceptionHandle()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> StartUpate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UnhandlePlatformExceptionHandle()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> WaitAppExit()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
