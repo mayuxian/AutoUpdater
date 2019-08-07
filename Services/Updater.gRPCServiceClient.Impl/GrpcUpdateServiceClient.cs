81372fdd455cbfb001534a5272a0279eaeba3e4d
mayuxian <415705683@qq.com>
2019-08-07 20:09:29 +0800

    重命名grpc的客户端实现的工程名。有xxx.gRPCService.xxx改为了xxx.gRPCServiceClient.xxx

---
+++ Services/Updater.gRPCServiceClient.Impl/GrpcUpdateServiceClient.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Threading;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Updater.GRPCService.Protocol;
+
+namespace Updater.gRPCService.Impl
+{
+    public class GrpcUpdateServiceClient : IUpdateService.IUpdateServiceClient
+    {
+        public EventHandler<GrpcErrorEventArgs> GrpcCommunicationError;
+
+        public GrpcUpdateServiceClient(Channel channel)
+            :base(channel)
+        {
+        
+        }
+
+        public override GRPCService.Protocol.Response GetResponseAsync(Request request, CallOptions options)
+        {
+          return  base.GetResponseAsync(request, options);
+        }
+        public override GRPCService.Protocol.Response GetResponseAsync(Request request, Metadata headers = null, DateTime? deadline = null, CancellationToken cancellationToken = default)
+        {
+            return base.GetResponseAsync(request, headers, deadline, cancellationToken);
+        }
+        public override AsyncUnaryCall<GRPCService.Protocol.Response> GetResponseAsyncAsync(Request request, CallOptions options)
+        {
+            return base.GetResponseAsyncAsync(request, options);
+        }
+
+        public override AsyncUnaryCall<GRPCService.Protocol.Response> GetResponseAsyncAsync(Request request, Metadata headers = null, DateTime? deadline = null, CancellationToken cancellationToken = default)
+        {
+            return base.GetResponseAsyncAsync(request, headers, deadline, cancellationToken);
+        }
+
+        public override AsyncServerStreamingCall<GRPCService.Protocol.Response> GetResponseStreamAsync(Request request, CallOptions options)
+        {
+            return base.GetResponseStreamAsync(request, options);
+        }
+
+        public override AsyncServerStreamingCall<GRPCService.Protocol.Response> GetResponseStreamAsync(Request request, Metadata headers = null, DateTime? deadline = null, CancellationToken cancellationToken = default)
+        {
+            return base.GetResponseStreamAsync(request, headers, deadline, cancellationToken);
+        }
+    }
+
+    public class GrpcErrorEventArgs : EventArgs
+    {
+        /// <summary>
+        /// 错误
+        /// </summary>
+        public Exception Exception { get; private set; }
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="exception"></param>
+        public GrpcErrorEventArgs(Exception exception)
+        {
+            this.Exception = exception;
+        }
+    }
+}
---
+++ Services/Updater.gRPCServiceClient.Impl/UpdateService.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Updater.CommService.Interface;
+using Updater.GRPCService.Protocol;
+
+namespace Updater.gRPCService.Impl
+{
+    public class UpdateService //: ICommService
+    {
+        //private static Server _server;
+        private static Channel _channel;
+        //private IUpdateService.IUpdateServiceClient _updateServiceClient;
+        private GrpcUpdateServiceClient _updateServiceClient;
+        private CommOptions _commOptions;
+
+        public UpdateService()
+        {
+            //_updateServiceClient = new IUpdateService.IUpdateServiceClient(_channel);
+            //Collection<ChannelOption> list = new Collection<ChannelOption>();
+            //ChannelOption channelOption = new ChannelOption(ChannelOptions.SoReuseport)
+
+            _channel = new Channel("", ChannelCredentials.Insecure, null);
+            _updateServiceClient = new GrpcUpdateServiceClient(_channel);
+            //_channel.ConnectAsync(null);
+            //_channel.State==ChannelState.TransientFailure
+        }
+
+        public Task<Response> GetAsync(string url, CommOptions setting = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<byte[]> GetBytesAsync(string url, CommOptions setting = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<string> GetStringAsync(string url, CommOptions setting = null)
+        {
+            return await GetRpcStringAsync(url);
+        }
+
+        public Task<Response> PostAsync(string url, CommOptions setting = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        private async Task<string> GetRpcStringAsync(string url)
+        {
+            var response = _updateServiceClient.GetResponseAsync(new GRPCService.Protocol.Request(), new CallOptions());
+            //response.Content
+
+            //EventHandler<GrpcErrorEventArgs> errorHandler = null;
+            //errorHandler = (sender, e) =>
+            //{
+            //    _updateServiceClient.GrpcCommunicationError -= errorHandler;
+            //    throw e.Exception;
+            //};
+            //_updateServiceClient.GrpcCommunicationError += errorHandler;
+
+            //var result = await grpcUpdateService.GetResponseAsync(new GRPCService.Protocol.Request(), null);
+            ////TODO:需要识别，出异常后返回，还是未出异常返回。
+            //return result.Content.ToString();
+            return await Task.FromResult<string>(null);
+        }
+
+        private async Task<bool> GetRpcStreamAsync(string url)
+        {
+            return await Task.FromResult<bool>(false);
+        }
+    }
+
+    public class Response : IResponse<object>
+    {
+        /// <summary>
+        /// 是否成功
+        /// </summary>
+        public bool Success { get; set; }
+
+        /// <summary>
+        /// 结果
+        /// </summary>
+        public object Result { get; set; }
+
+        /// <summary>
+        /// 异常信息
+        /// </summary>
+        public IResponseError Error { get; set; }
+    }
+}
