98f47c988130a8cf2cd984e15e82638fc4bbaea8
mayuxian <415705683@qq.com>
2019-08-07 20:48:03 +0800

    添加grpc的服务端的server工程

---
+++ Tests/Updater.gRPCService.Server/GrpcService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Grpc.Core;
+using Updater.GRPCService.Protocol;
+
+namespace Updater.gRPCService.Server
+{
+   public class GrpcService
+    {
+        private static Grpc.Core.Server _server;
+
+        public static void Startup()
+        {
+            _server = new Grpc.Core.Server
+            {
+                Services = { IUpdateService.BindService(new GrpcUpdateServer()) },
+                Ports = { new ServerPort("localhost", 40001, ServerCredentials.Insecure) }
+            };
+            _server.Start();
+
+
+            Console.WriteLine("grpc ServerListening On Port 40001");
+            Console.WriteLine("任意键退出...");
+            Console.ReadKey();
+
+            _server?.ShutdownAsync().Wait();
+        }
+    }
+}
---
+++ Tests/Updater.gRPCService.Server/GrpcUpdateServer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Updater.GRPCService.Protocol;
+
+namespace Updater.gRPCService.Server
+{
+    public class GrpcUpdateServer : IUpdateService.IUpdateServiceBase
+    {
+        public override Task<RpcResponse> GetResponse(RpcRequest request, ServerCallContext context)
+        {
+            return base.GetResponse(request, context);
+        }
+
+        public override Task GetResponseStream(RpcRequest request, IServerStreamWriter<RpcResponse> responseStream, ServerCallContext context)
+        {
+            return base.GetResponseStream(request, responseStream, context);
+        }
+    }
+}
