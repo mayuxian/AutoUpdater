a29b65b4ddf750434d26148fbe4c526c8e843b59
mayuxian <415705683@qq.com>
2019-08-08 12:29:01 +0800

    调整目录结构

--- Sample/MainAppTest/MainWindow.xaml.cs
+++ Sample/MainAppTest/MainWindow.xaml.cs
@@ -12,7 +12,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using Updater.gRPCService.Impl;
 
 namespace MainAppTest
 {
@@ -24,13 +23,6 @@ namespace MainAppTest
         public MainWindow()
         {
             InitializeComponent();
-            Startup();
-        }
-
-        public async void Startup()
-        {
-            UpdateService serive = new UpdateService();
-            var result = await serive.PostAsync(null, null);
         }
     }
 }
---
+++ Sample/ServiceTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ServiceTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
